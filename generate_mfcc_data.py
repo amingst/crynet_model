75a9be92c72de7b66a7be4f128a290dbdc9d8b45
Andrew Mingst <amingst97@gmail.com>
2020-04-21 16:59:20 -0400

    Created generate_mfcc_data.py
    generate_mfcc_data.py:
    Loads audio_data from a pickle file and generates a dataframe containing mfccs, scaled_mfccs, and the class_label. That dataframe is then serialized into a pickle file

---
+++ generate_mfcc_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import librosa
+
+#TODO: Figure out why param n_mfcc doesn't work
+def generate_mfcc_data(data_path):
+    mfcc_data = []
+    audio_df = pd.read_pickle(data_path)
+
+    for row in audio_df.iterrows():
+        current_row = row[1]
+
+        mfccs = librosa.feature.melspectrogram(y=current_row.samples, sr=current_row.sample_rate, S=None)
+        scaled_mfccs = np.mean(mfccs.T, axis=0)
+
+        mfcc_data.append([mfccs, scaled_mfccs, current_row.class_label])
+    
+    df = pd.DataFrame(mfcc_data, columns=["mfccs", "scaled_mfccs", "class_label"])
+    df.to_pickle('mfcc_data.pkl')
+
+    print("Saved to pickle file in root directory")
+    return None
+
+generate_mfcc_data('audio_data.pkl')
